08ba896e006f194062c899e223ee3fc9848b4195
kathrynmckean <121512579+kathrynmckean@users.noreply.github.com>
2023-06-21 14:50:31 -0500

    Update app.js
    Cleaning up the file, commenting out console.log checks

--- app.js
+++ app.js
@@ -1,21 +1,20 @@
 async function getGif() {
-    // let userInput = document.getElementById("input").value
     let userInput = document.getElementById("input").value
-    console.log(userInput)
+    // console.log(userInput)
 
     let giphyApiKey = 'wL2VhWAS3RK5vRLV4Xur1HwpaF9O3dpp'
     let giphyApiURL = `https://api.giphy.com/v1/gifs/search?q=${userInput}&rating=pg-13&api_key=${giphyApiKey}`
 
     const response = await fetch(giphyApiURL)
-    console.log(response)
+    // console.log(response)
 
     const data = await response.json();
-    console.log(data)
+    // console.log(data)
 
     let num = Math.floor((Math.random() * 25))
     //random number between 0 and 24  
     let imgPath = data.data[num].images.fixed_height.url
-    console.log(imgPath)
+    // console.log(imgPath)
     document.getElementById("gif").src = imgPath; // updates the gif to the most current search
 }
 
